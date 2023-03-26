6e5a942d06390b0e54e6614a9fdfae5042c3ddf0
v_ghasemi <v_ghasemi@csdiran.ir>
2023-03-26 10:49:55 +0330

    Complete Customrt CRUD CQRS

---
+++ Application/Features/Commands/Customers/CreateCustomer/CreateCustomerCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces;
+using Domain.DTOs.Responses.Customers;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.Commands.Customers.CreateCustomer;
+
+public class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand, CreateCustomerDto>
+{
+	private readonly IApplicationDbContext _context;
+	public CreateCustomerCommandHandler(IApplicationDbContext context)
+	{
+		_context = context;
+	}
+	public async Task<CreateCustomerDto> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
+	{
+		var customer = request.MapTo();
+		var entityEntry =
+			await _context.Customers.AddAsync(customer);
+
+		var resultId = await _context.SaveChangesAsync();
+
+		return new CreateCustomerDto(id: resultId);
+	}
+}
