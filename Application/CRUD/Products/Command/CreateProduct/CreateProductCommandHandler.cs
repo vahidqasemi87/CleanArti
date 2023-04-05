f90e244bbce0dded9c856717982fb64b3ea0144b
v_ghasemi <v_ghasemi@csdiran.ir>
2023-04-05 15:23:31 +0330

    Final Create Oriduct

--- Application/CRUD/Products/Command/CreateProduct/CreateProductCommandHandler.cs
+++ Application/CRUD/Products/Command/CreateProduct/CreateProductCommandHandler.cs
@@ -1,5 +1,8 @@
-﻿using Application.Interfaces;
+﻿using Application.Common.Interfaces.Learning;
+using Application.Interfaces;
+using AutoMapper;
 using Domain.DTOs.Responses.Products;
+using Domain.Entities;
 using FluentValidation;
 using MediatR;
 using System.Collections.Generic;
@@ -11,30 +14,31 @@ namespace Application.Features.Products.Command.CreateProduct;
 
 public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductDto>
 {
-    private readonly IApplicationDbContext _context;
+    //private readonly IApplicationDbContext _context;
+    private readonly IUnitOfWork_New _unitOfWork_New;
+    private readonly IRepository_New<Product> _productRepository;
+    private readonly IMapper _mapper;
     private readonly IEnumerable<IValidator> _validators;
-    public CreateProductCommandHandler(IApplicationDbContext context, IEnumerable<IValidator<CreateProductCommand>> validators)
+    public CreateProductCommandHandler( IEnumerable<IValidator<CreateProductCommand>> validators, IUnitOfWork_New unitOfWork_New, IRepository_New<Product> repository_New, IMapper mapper)
     {
-        _context = context;
         _validators = validators;
+        _unitOfWork_New = unitOfWork_New;
+        _productRepository = repository_New;
+        _mapper = mapper;
     }
     public async Task<CreateProductDto> Handle(CreateProductCommand request, CancellationToken cancellationToken)
     {
         var tt = _validators.Any();
 
-        var movie = request.MapTo();
-        var entityEntry = _context.Products.Add(movie);
-        var result = await _context.SaveChangesAsync();
+        //var movie = request.MapTo();
+        var product = new Product();
+
+		product = _mapper.Map<Product>(request);
+
+        _productRepository.Add(product);
+         var result = await _unitOfWork_New.SaveChangesAsync();
         return new CreateProductDto(id: result);
     }
 
 
 }
-
-//public class CreateProductPiplene<CreateProductCommand, CreateProductDto> : IPipelineBehavior<CreateProductCommand, CreateProductDto>
-//{
-//	public Task<CreateProductDto> Handle(CreateProductCommand request, RequestHandlerDelegate<CreateProductDto> next, CancellationToken cancellationToken)
-//	{
-//		throw new System.NotImplementedException();
-//	}
-//}
