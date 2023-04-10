b4b5747c5a71fb05363a37d3470b7546675571a3
v_ghasemi <v_ghasemi@csdiran.ir>
2023-04-10 09:01:36 +0330

    Validation correct Place

---
+++ Application/CRUD/Orders/Command/CreateOrder/OrderFluentValidation.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using FluentValidation;
+
+namespace Application.CRUD.Orders.Command.CreateOrder;
+
+public class OrderFluentValidation : AbstractValidator<Order>
+{
+    public OrderFluentValidation()
+    {
+        RuleFor(c => c.IsPayed)
+            .NotNull()
+            .NotEmpty();
+
+        RuleFor(c => c.IsSend)
+            .NotNull()
+            .NotEmpty();
+    }
+}
